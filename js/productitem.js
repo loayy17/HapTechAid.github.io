5bdd21fe22736e96516b3c523ddef89dbd058e0b
loayy17 <loay1alhindi1@gmail.com>
2023-08-13 07:32:31 +0300

    sec commit

--- js/productitem.js
+++
@@ -1,9 +0,0 @@
-
-var detail ={
-    "item1":{
-        "title":"Commode Chair",
-        "description":"The preferred choice of a vast range of acclaimed DJs. Punchy, bass-focused sound and high isolation. Sturdy headband and on-ear cushions suitable for live performance",
-        "Img":"../Products/images/CommodeChairBlue.png"
-    }
-}
-export default detail
